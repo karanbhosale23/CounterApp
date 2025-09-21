0946272698143f868992ad9ef6946fce73c3d0e0
Karan Bhosale <bhosalekaran252@gmail.com>
2025-09-21 20:59:12 +0530

    Change color and font size

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import './Counter.css'
+import React from 'react';
+import './Counter.css';
 
 class Counter extends React.Component {
     constructor(props) {
-        super(props)
-        this.state = { count: 0 }
+        super(props);
+        this.state = { count: 0 };
 
-        // bind methods (so `this` works)
-        this.increase = this.increase.bind(this)
-        this.decrease = this.decrease.bind(this)
-        this.resetCount = this.resetCount.bind(this)
+        this.increase = this.increase.bind(this);
+        this.decrease = this.decrease.bind(this);
+        this.resetCount = this.resetCount.bind(this);
     }
 
     increase() {
-        this.setState(prev => ({ count: prev.count + 1 }))
+        this.setState(prev => ({ count: prev.count + 1 }));
     }
 
     decrease() {
-        this.setState(prev => ({ count: prev.count - 1 }))
+        this.setState(prev => ({ count: prev.count - 1 }));
     }
 
     resetCount() {
-        this.setState({ count: 0 })
+        this.setState({ count: 0 });
     }
 
     render() {
         return (
             <div className="counter">
-                <h1 id="count">{this.state.count}</h1>
+                <h1 id="count" className={this.state.count !== 0 ? 'glow' : ''}>
+                    {this.state.count}
+                </h1>
                 <div className="btns">
-                    <button className="increase" onClick={this.increase}>Increase</button>
-                    <button className="decrease" onClick={this.decrease}>Decrease</button>
-                    <button className="reset" onClick={this.resetCount}>Reset</button>
+                    <button className="increase glow-btn" onClick={this.increase}>Increase</button>
+                    <button className="decrease glow-btn" onClick={this.decrease}>Decrease</button>
+                    <button className="reset glow-btn" onClick={this.resetCount}>Reset</button>
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default Counter
+export default Counter;
